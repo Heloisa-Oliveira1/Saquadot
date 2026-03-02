6f63a45ee5e3a7c894dad45e0dfcf5c125506051
Heloisa <heloisasilvahh12@gmail.com>
2026-03-02 20:31:32 -0300

    Migrando para PostgreSQL

--- saquadot/settings.py
+++ saquadot/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import os
+import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -64,10 +65,11 @@ WSGI_APPLICATION = 'saquadot.wsgi.application'
 
 # DATABASE
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 # PASSWORD VALIDATION
